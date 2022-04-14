541349432e354b045a8ee5ea1111888a08a8a881
Kyle <mcgroderk@gmail.com>
2022-04-14 16:12:46 +0900

    Added HTML-CSS-JS files, math functions, HTML tags

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = function (num1, num2) {
+    return num1 + num2;
+}
+
+const subtract = function (num1, num2) {
+    return num1 - num2;
+}
+
+const multiply = function (num1, num2) {
+    return num1 * num2;
+}
+
+const divide = function (num1, num2) {
+    return num1 / num2;
+}
+
+const operate = function (operator, num1, num2) {
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === '*') return multiply(num1, num2);
+    if (operator === '/') return divide(num1, num2);
+}
