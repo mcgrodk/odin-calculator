3f087e5b8bdd9cb64eef9de704133a45520b7f12
Kyle <mcgroderk@gmail.com>
2022-04-25 15:55:19 +0900

    Calculator displays number input up to 10 digits

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const calcDisplay = document.querySelector('#calc-display');
+const clearBtn = document.querySelector('#btn-ac');
 const buttons = document.querySelectorAll('button');
+
+let displayValue = document.createElement('p');
+calcDisplay.appendChild(displayValue);
+
+displayValue.textContent = '0';
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (displayValue.textContent === '0') displayValue.textContent = '';
+    if (displayValue.textContent.length >= 10)  {
+      return;
+    } else displayValue.textContent += button.value;
+});
+});
+
+clearBtn.addEventListener('click', () => {
+  displayValue.textContent = '0';
+});
+
 
 const add = function (num1, num2) {
     return num1 + num2;
@@ -23,3 +43,4 @@ const operate = function (operator, num1, num2) {
     if (operator === '*') return multiply(num1, num2);
     if (operator === '/') return divide(num1, num2);
 }
+
