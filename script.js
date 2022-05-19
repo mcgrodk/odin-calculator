b1b83bf87bddff75e38ed74dbc7edb7347f1d778
Kyle <mcgroderk@gmail.com>
2022-05-19 16:06:29 +0900

    Reorganized script, refactored functions, deleted unneeded value tags in HTML

--- script.js
+++ script.js
@@ -1,69 +1,43 @@
-// Status:
-// Calc can return integer operations after hitting equals
-// Calc cannot chain operations without hitting equals
-// Calc breaks when hitting zero after first operation
-// Calc can't handle decimal operations
-
 const display = document.querySelector('#display');
-display.textContent = '0';
-
 const btnClear = document.querySelector('#btn-clear');
-btnClear.addEventListener('click', clear);
-
-const btnEquals = document.querySelector('#btn-equals');
-btnEquals.addEventListener('click', () => {
-  if (newOperand === null) return;
-  operate();
-  operators.forEach((operator) => {
-    operator.classList.remove('current-op');
-    })
-  calcTotal = display.textContent;
-  newOperand = null;
-});
-
 const btnDel = document.querySelector('#btn-del');
-btnDel.addEventListener('click', () => {
-  if (display.textContent === 'Nope!') {
-    clear();
-  }
-  else display.textContent = deleteLastChar(display.textContent);
-});
-
 const btnDecimal = document.querySelector('#btn-decimal');
-btnDecimal.addEventListener('click', () => {
-  if(display.textContent.includes('.') == true) {
-    return;
-  } else display.textContent += '.';
-});
-
-let calcTotal = null;
-let newOperand = null;
-let currentOperator = null;
+const btnEquals = document.querySelector('#btn-equals');
+const btnAdd = document.querySelector('#btn-add');
+const btnSubtract = document.querySelector('#btn-subtract');
+const btnMultiply = document.querySelector('#btn-multiply');
+const btnDivide = document.querySelector('#btn-divide');
 
 const numbers = document.querySelectorAll('.number');
-numbers.forEach((number) => {
-  number.addEventListener('click', () => {
-    if (display.textContent === '0') display.textContent = '';
-    
-    if (calcTotal !== null) {
-      display.textContent = '';
-      newOperand = display.textContent;
-    }
-    
-    if (display.textContent.length >= 11)  {
-      return;
-    } else display.textContent += number.value;
-});
-});
-
 const operators = document.querySelectorAll('.operator');
-operators.forEach((operator) => {
-  operator.addEventListener('click', () => {
-    calcTotal = Number(display.textContent);
-    currentOperator = (operator.id);
-    operator.classList.add('current-op');
-  })
-});
+
+function add(num1, num2) {
+  return  num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate() {
+  newOperand = Number(display.textContent);
+  if (currentOperator === 'btn-add') display.textContent = parseFloat(add(calcTotal, newOperand));
+  if (currentOperator === 'btn-subtract') display.textContent = parseFloat(subtract(calcTotal, newOperand));
+  if (currentOperator === 'btn-multiply') display.textContent = parseFloat(multiply(calcTotal, newOperand));
+  if (currentOperator === 'btn-divide') {
+    if (newOperand === 0) {
+      display.textContent = 'Nope!'; }
+      else display.textContent = parseFloat(divide(calcTotal, newOperand));
+}
+}
 
 function clear() {
   display.textContent = 0;
@@ -75,69 +49,81 @@ function clear() {
 }
 
 function deleteLastChar (string) {
-  let delString = string.slice(0, -1); 
-  if (string.length == 1) {
-    return 0;
-  }
-    else return delString;
+  if (string === 'Nope!' || string.length === 1) {
+    clear();
+  } else return string.slice(0, -1);
 }
 
-function add(num1, num2) {
-    let result = num1 + num2;
-    return result;
-}
+let calcTotal = null;
+let newOperand = null;
+let currentOperator = null;
+display.textContent = '0';
 
-function subtract(num1, num2) {
-    let result = num1 - num2;
-    return result;
-}
+btnClear.addEventListener('click', clear);
 
-function multiply(num1, num2) {
-    let result = num1 * num2;
-    return result;
-}
+btnDel.addEventListener('click', () => {
+  display.textContent = deleteLastChar(display.textContent);
+});
 
-function divide(num1, num2) {
-    let result = num1 / num2;
-    return result;
-}
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
+    if (display.textContent.length >= 11) return;
+    if (display.textContent === '0') display.textContent = '';
+    
+    if (calcTotal !== null) {
+      display.textContent = '';
+      newOperand += display.textContent;
+    } 
+    
+    display.textContent += number.innerText;
+});
+});
 
-function operate() {
-  newOperand = Number(display.textContent);
-  console.log(calcTotal);
-  console.log(newOperand);
-  if (currentOperator === 'btn-add') display.textContent = parseFloat(add(calcTotal, newOperand));
-  if (currentOperator === 'btn-subtract') display.textContent = parseFloat(subtract(calcTotal, newOperand));
-  if (currentOperator === 'btn-multiply') display.textContent = parseFloat(multiply(calcTotal, newOperand));
-  if (currentOperator === 'btn-divide') {
-    if (newOperand === 0) {
-      display.textContent = 'Nope!'; }
-      else display.textContent = parseFloat(divide(calcTotal, newOperand));
-}
-}
+btnDecimal.addEventListener('click', () => {
+  if(display.textContent.includes('.') == true) {
+    return;
+  } else display.textContent += '.';
+});
 
-const btnAdd = document.querySelector('#btn-add');
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    calcTotal = Number(display.textContent);
+    currentOperator = (operator.id);
+    operator.classList.add('current-op');
+  })
+});
+
+btnEquals.addEventListener('click', () => {
+  if (newOperand === null) return;
+  operate();
+  operators.forEach((operator) => {
+    operator.classList.remove('current-op');
+    })
+  calcTotal = display.textContent;
+  newOperand = null;
+});
+
 btnAdd.addEventListener('click', () => {
   btnSubtract.classList.remove('current-op');
   btnMultiply.classList.remove('current-op');
   btnDivide.classList.remove('current-op');
 })
 
-const btnSubtract = document.querySelector('#btn-subtract');
+
 btnSubtract.addEventListener('click', () => {
   btnAdd.classList.remove('current-op');
   btnMultiply.classList.remove('current-op');
   btnDivide.classList.remove('current-op');
 })
 
-const btnMultiply = document.querySelector('#btn-multiply');
+
 btnMultiply.addEventListener('click', () => {
   btnAdd.classList.remove('current-op');
   btnSubtract.classList.remove('current-op');
   btnDivide.classList.remove('current-op');
 })
 
-const btnDivide = document.querySelector('#btn-divide');
+
 btnDivide.addEventListener('click', () => {
   btnAdd.classList.remove('current-op');
   btnSubtract.classList.remove('current-op');
